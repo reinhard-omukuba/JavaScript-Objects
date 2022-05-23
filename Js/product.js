e9f3e00209befb6265901a80fc9d348968f9fc30
reinhard-omukuba <reinhardomukuba@gmail.com>
2022-05-23 11:13:13 +0300

    new changes

--- Js/product.js
+++ Js/product.js
@@ -24,6 +24,11 @@ if(theID == Nike.id){
     document.getElementById("receivedName").innerText = Nike.Name;
     document.getElementById("receivedDesc").innerText = Nike.desc;
     document.getElementById("receivedPrice").innerText = "KES. " + Nike.Price;
+    
+    var price = Nike.Price;
+
+    totalPrice(price);
+
 
 }else if(theID == Bata.id){
 
@@ -31,4 +36,133 @@ if(theID == Nike.id){
     document.getElementById("receivedName").innerText = Bata.Name;
     document.getElementById("receivedDesc").innerText = Bata.desc;
     document.getElementById("receivedPrice").innerText = "KES. " + Bata.Price;
+
+    var price = Bata.Price;
+    totalPrice(price);
 }
+
+
+
+function totalPrice(price){
+
+    document.getElementById("grandPrice").innerText = "KES." + price;
+
+    document.getElementById("qty").onkeyup = function(){
+        var qty = document.getElementById("qty").value;
+        var qtyInt = parseInt(qty);
+        var total = qtyInt * price;
+
+        if(qty == ""){
+            document.getElementById("grandPrice").innerText = "KES." + price;
+            deliveryPrice(price);
+        }else{
+            document.getElementById("grandPrice").innerText = "KES." + total;
+            deliveryPrice(total);
+        }       
+    }
+
+    deliveryPrice(price);
+}
+
+function deliveryPrice(steve){
+
+    console.log(steve)
+
+    var itemPrice = parseInt(steve)
+
+    document.getElementById("paypal-button-container").innerHTML = "";
+    payNow(itemPrice);
+    
+
+
+    document.getElementById("city").onchange = function(){
+        var city = document.getElementById("city").value;
+        if(city == "Kakamega"){
+            document.getElementById("zoneB").style.display = "block";
+            document.getElementById("zoneA").style.display = "none";
+        }else if(city == "Nairobi"){
+            document.getElementById("zoneA").style.display = "block";
+            document.getElementById("zoneB").style.display = "none";
+        }
+    }
+
+    //
+    document.getElementById("zoneA").onchange = function(){
+        var zoneA = document.getElementById("zoneA").value;
+
+        if(zoneA == "Kilimani"){
+            var delPrice = 100;
+            var theTotal = delPrice + itemPrice
+            document.getElementById("grandPrice").innerText = "KES." + theTotal;
+
+            document.getElementById("paypal-button-container").innerHTML = "";
+            payNow(theTotal)
+
+        }else if(zoneA == "Lavington"){
+            var delPriceB = 150;
+            var theTotalB = delPriceB + itemPrice
+            document.getElementById("grandPrice").innerText = "KES." + theTotalB;
+
+            document.getElementById("paypal-button-container").innerHTML = "";
+            payNow(theTotalB)
+        }
+
+    }
+
+    document.getElementById("zoneB").onchange = function(){
+        var zoneA = document.getElementById("zoneB").value;
+
+        if(zoneA == "Kimilili"){
+            var delPrice = 700;
+            var theTotal = delPrice + itemPrice
+            document.getElementById("grandPrice").innerText = "KES." + theTotal;
+
+            document.getElementById("paypal-button-container").innerHTML = "";
+            payNow(theTotal)
+
+        }else if(zoneA == "Bahati"){
+            var delPriceB = 850;
+            var theTotalB = delPriceB + itemPrice
+            document.getElementById("grandPrice").innerText = "KES." + theTotalB;
+
+            document.getElementById("paypal-button-container").innerHTML = "";
+            payNow(theTotalB)
+        }
+
+    }
+
+}
+
+
+function payNow(recAmount){
+
+    //converting into usd
+    var amtUsd = recAmount / 100;
+
+    ///
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+          // Set up the transaction
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: amtUsd
+              }
+            }]
+          });
+        },
+
+        onApprove: function(data, actions) {
+            alert('Payment Sucessful');
+
+        },
+
+        onCancel: function (data) {
+            alert('payment not successful')
+        }
+
+
+
+      }).render('#paypal-button-container');
+}
+
